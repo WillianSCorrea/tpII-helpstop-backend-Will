25bb3101a0fb1a882f4b9ba6bfbc5257321e66c2
Willian Da Silva Correa <willianwcorrea@gmail.com>
2025-02-24 12:34:07 -0300

    Criação basica dos dominios de Entidade

---
+++ HelpApp/HelpApp.Domain/Entity/Category.cs
@@ -0,0 +1,34 @@
+﻿
+using HelpApp.Domain.Validation;
+using System.Collections.Generic;
+
+
+namespace HelpApp.Domain.Entity
+{
+    public class Category
+    {
+        #region Atributos 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        #endregion
+
+        #region Construtores
+        public Category(string name)
+        {
+
+            ValidateDomain(name);
+        }
+        
+        public ICollection<Product> Products { get; set; }
+
+        #endregion
+
+        #region
+        private void ValidateDomain(string Name)
+        {
+            DomainExcpetionValidation.When(string.IsNullOrEmpty(Name), "Invalid Name , Name is Required");
+            DomainExcpetionValidation.When(Name.Length < 3, "Invalid Name , too short , minimum 3 characters");
+        }
+        #endregion
+    }
+}
---
+++ HelpApp/HelpApp.Domain/Validation/DomainExcpetionValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelpApp.Domain.Validation
+{
+    public class DomainExcpetionValidation : Exception
+    {
+        public DomainExcpetionValidation(string error) : base(error) { }
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+            {
+                throw new DomainExcpetionValidation(error);
+            }
+        }
+    }
+}
